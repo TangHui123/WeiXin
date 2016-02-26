654289b2d9a54e9ea7ec3d70d687cc00b551c2a4
tanghui <839283929@qq.com>
2016-02-26 15:16:35 +0800

    weixin

---
+++ weitest/src/main/java/com/talkweb/tanghui/weitest/Tang.java
@@ -0,0 +1,12 @@
+package com.talkweb.tanghui.weitest;
+
+public class Tang {
+    public int anInt;
+    public Tang(){
+        anInt = 1;
+    }
+    
+    public void testTang() {
+        anInt = 2;
+    }
+}
